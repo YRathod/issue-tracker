6569251ada3731dddf83342de2e4bfa5a09f1b08
YRathod <YRathod@users.noreply.github.com>
2023-12-12 20:36:00 -0500

    Add Login Logout Links

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,10 +4,12 @@ import { usePathname } from "next/navigation";
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
 const currentPath = usePathname();
-console.log(currentPath);
+const {status, data: session} = useSession();
 
 const nav = [
                 {href:'/', label: 'Dashboard'},
@@ -17,8 +19,9 @@ const nav = [
     <div className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
         <Link href={"/"}><AiFillBug></AiFillBug></Link>
         <ul className="flex space-x-6">
-            {nav.map(item => 
-                <Link key={item.href} 
+            {nav.map(item =>
+                <li key={item.href}>
+                <Link 
                     className={
                         classNames({
                             'text-zinc-900':  currentPath === item.href,
@@ -26,8 +29,13 @@ const nav = [
                             'hover:text-zinc-800 transition-colors': true
                         })}
                     href={item.href}>{item.label}</Link>
+                </li>
             )}
         </ul>
+        <Box>
+            { status === 'authenticated' && (<Link href="/api/auth/signout">Log out</Link>)}
+            { status === 'unauthenticated' && (<Link href="/api/auth/signin">Log In</Link>)}
+        </Box>
     </div>
   )
 }
