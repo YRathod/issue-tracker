a3f07382cfefd4fb70d61416d908377c41a8a2ca
YRathod <YRathod@users.noreply.github.com>
2023-12-13 12:37:15 -0500

    Add a loading skeleton

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
-
+import { Avatar, Box, DropdownMenu, Flex, Text } from "@radix-ui/themes";
+import {Skeleton} from '../Components/Skeleton';
 
 const NavBar = () => {
   return (
@@ -47,7 +47,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
     const {status, data: session} = useSession();
 
-    if(status === "loading") return null;
+    if(status === "loading") return <Skeleton width="3rem"></Skeleton>;
 
     if(status === 'unauthenticated')
      return <Link className="nav-link" href="/api/auth/signin">Login</Link>
