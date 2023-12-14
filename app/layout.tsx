9a167daba717f69586170585fea1660311e71c24
YRathod <YRathod@users.noreply.github.com>
2023-12-14 12:47:10 -0500

    Setup React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import NavBar from './Components/NavBar'
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -25,17 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme appearance='light' accentColor='violet'>
-            <NavBar></NavBar> 
-            <main className='p-5'>
-                <Container>
-                    {children}
-                </Container>
-              </main>
-            {false && <ThemePanel></ThemePanel>}
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme appearance='light' accentColor='violet'>
+              <NavBar></NavBar> 
+              <main className='p-5'>
+                  <Container>
+                      {children}
+                  </Container>
+                </main>
+              {false && <ThemePanel></ThemePanel>}
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
